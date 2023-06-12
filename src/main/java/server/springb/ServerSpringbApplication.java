15f7e3288db0a6768b5b48f5668c6d277faacd53
Bugtok14 <130125546+bugtok14@users.noreply.github.com>
2023-06-12 14:49:15 +0800

    Init commit

---
+++ src/main/java/server/springb/ServerSpringbApplication.java
@@ -0,0 +1,13 @@
+package server.springb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerSpringbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerSpringbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/springb/ServerSpringbApplicationTests.java
@@ -0,0 +1,13 @@
+package server.springb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSpringbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
